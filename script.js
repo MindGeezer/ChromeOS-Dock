8f28dcf7ce3d25a5b484ad82e02b48c488dd59f2
LagTheSystem <53086599+LagTheSystem@users.noreply.github.com>
2023-12-01 21:43:39 +0000

    Completed initial version

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+// Calling showTime function at every second
+setInterval(showTime, 1000);
 
+// Defining showTime funcion
+function showTime() {
+    // Getting current time and date
+    let time = new Date();
+    let month = time.getMonth();
+    let date = time.getDate();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+  
+    time.setMonth(month);
+    month = time.toLocaleString('en-US', { month : 'short' })
+  
+    // Setting time for 12 Hrs format
+    if (hour >= 12) {
+        if (hour > 12) hour -= 12;
+    } else if (hour == 0) {
+        hr = 12;
+    }
+ 
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+    hour = parseInt(hour, 10);
+    
+    let currentTime =
+        hour +
+        ":" +
+        min;
+  
+    let currentDate = month + " " + date
+ 
+    // Displaying the time
+ document.getElementById("clock").innerHTML = currentTime;
+  document.getElementById("calendar").innerHTML = currentDate;
+}
+ 
+showTime();
